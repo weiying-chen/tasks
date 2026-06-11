028a29a992085a0801ad3598fc95c210cf3cd93e
Wei-ying Chen <weiying.alex.chen@gmail.com>
2026-06-11 22:29:10 +0800

    Remove assignment message spaces

--- tests/test_create_message.py
+++ tests/test_create_message.py
@@ -332,8 +332,8 @@ class CreateMessageTests(unittest.TestCase):
         message = create_message.create_message(tasks, msg_type="task-assignment")
         self.assertEqual(
             message,
-            "請@Shawn edit + 定稿三集大愛醫生館（不是潰瘍的十二指腸出血 + 壯年出血在腦內 + 腎癌迷走下腔靜脈），"
-            "片長共6分04秒，預計 edit + 定稿3時02分，deadline等手上工作完成後再給，謝謝~",
+            "請@Shawnedit + 定稿三集大愛醫生館（不是潰瘍的十二指腸出血 + 壯年出血在腦內 + 腎癌迷走下腔靜脈），"
+            "片長共6分04秒，預計edit + 定稿3時02分，deadline等手上工作完成後再給，謝謝~",
         )
 
     def test_subs_summary_message_requires_parenthesized_episode_titles(self):
